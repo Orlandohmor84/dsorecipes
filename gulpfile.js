f4f58e4a5b38aa512d87726db9887ac829088cd2
Orlando <orlando@smartforlife.com>
2018-07-31 13:02:44 -0400

    Login Design.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,11 @@ const gulp = require('gulp');
 const bootlint  = require('gulp-bootlint');
 const requireDir = require('require-dir');
 requireDir('./gulp-tasks/bootlint/');
-requireDir('./gulp-tasks/html/');
 requireDir('./gulp-tasks/less/');
 requireDir('./gulp-tasks/scripts/');
 
 //File paths
 const LESS_PATH = './src/assets/less/main.less';
-const SRC_PATH = './src/*.html';
-const INC_PATH = './src/includes/*.html';
 const SCRIPTS_CUSTOM_PATH = './src/assets/js/custom/*.js';
 
 //Watch
@@ -19,9 +16,8 @@ gulp.task('watch', ['default'], function() {
 	livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
 	gulp.watch(LESS_PATH, ['styles']);
-	gulp.watch(SRC_PATH, ['html']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'html'], function() {
+gulp.task('default', ['styles', 'scripts'], function() {
 	console.log('starting default task');
 });
