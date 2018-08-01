e27de8f7d9f96611af2c0aa47638fd61bda98e87
Orlando <orlando@smartforlife.com>
2018-08-01 11:30:27 -0400

    Recipe View Improvements.

--- app/models/index.js
+++ app/models/index.js
@@ -2,14 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RecipeSchema = new Schema({
-    name: String,
-    yield: String,
-    ing1name: String, 
-    ing1quant: Number,
+    category : String,
+    name : String,
+    flavor : String,
+    updated : String,
+    approver : String,
+    yield : String,
+    ing1name : String, 
+    ing1quant : Number,
     ing1unit : String,
+    ing1notes : String,
     ing2name: String, 
     ing2quant: Number,
-    ing2unit : String
+    ing2unit : String,
+    ing2notes : String,
+    ing3name: String, 
+    ing3quant: Number,
+    ing3unit : String,
+    ing3notes : String,
+    ing4name: String, 
+    ing4quant: Number,
+    ing4unit : String,
+    ing4notes : String,
+    ing5name: String, 
+    ing5quant: Number,
+    ing5unit : String,
+    ing5notes : String,
+    ing6name: String, 
+    ing6quant: Number,
+    ing6unit : String,
+    ing6notes : String
 });
 /*
 // the schema is useless so far
