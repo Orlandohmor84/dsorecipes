7c59f28cd095b92c5843805342a367eb7c912cfb
Orlando <orlando@smartforlife.com>
2018-08-16 13:29:30 -0400

    Added second table to recipes.

--- app/models/recipes.js
+++ app/models/recipes.js
@@ -10,108 +10,215 @@ const RecipeSchema = new Schema({
     updated : String,
     approver : String,
     yield : String,
-    ing01: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
+    table01 : {
+        ing01: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing02: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing03: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing04: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing05: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing06: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing07: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing08: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing09: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing10: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing11: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing12: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing13: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing14: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing15: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing16: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing17: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        }
     },
-    ing02: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing03: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing04: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing05: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing06: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing07: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing08: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing09: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing10: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing11: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing12: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing13: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing14: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing15: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing16: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    },
-    ing17: {
-        name : String, 
-        quant : Number,
-        unit : String,
-        notes : String
-    } 
+    table02 : {
+        ing01: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing02: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing03: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing04: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing05: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing06: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing07: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing08: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing09: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing10: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing11: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing12: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing13: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing14: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing15: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing16: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        },
+        ing17: {
+            name : String, 
+            quant : Number,
+            unit : String,
+            notes : String
+        }
+    }
+     
 });
 /*
 // the schema is useless so far
